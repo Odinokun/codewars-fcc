5b0fc3242af157a939906d10c1c2369fd9b87bfb
Odinokun <odinokun@gmail.com>
2021-10-09 17:00:18 +0300

    codewars task 012

---
+++ codewars/012/script.js
@@ -0,0 +1,10 @@
+function solution(str){
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
+
+console.log(solution('hello'));
+console.log(solution('world'));
