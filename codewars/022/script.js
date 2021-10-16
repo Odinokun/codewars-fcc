2f539e09a1d4637ff7f76bce46ee7432a746d403
Odinokun <odinokun@gmail.com>
2021-10-16 14:03:11 +0300

    codewars task 022

--- codewars/022/script.js
+++ codewars/022/script.js
@@ -1,1 +1,12 @@
-console.log('hello')
+//https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/javascript
+
+function firstNonConsecutive (arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] + 1 !== arr[i + 1]) {
+            return arr[i + 1];
+        }
+    }
+    return null;
+}
+
+console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]));
